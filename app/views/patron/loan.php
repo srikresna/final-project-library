da5f0823ae595d0d6a1653fbd91aa4a17d6e5635
srikresna <srikresna383@gmail.com>
2023-12-10 19:26:21 +0700

    chore: remove search functionality from loan view

--- app/views/patron/loan.php
+++ app/views/patron/loan.php
@@ -5,16 +5,6 @@
         <div class="col-md-6">
             <h2>Your Loan</h2>
         </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <div class="input-group">
-                    <input type="text" class="form-control" id="search" placeholder="Search">
-                    <div class="input-group-append">
-                        <button type="button" class="btn btn-primary" id="searchBtn">Search</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <table class="table">
