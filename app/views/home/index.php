2d1c509f1ead84af087e6095eca14c822fcad9b1
lenkaflorienkaa <lenkamelinda7@gmail.com>
2023-12-13 22:11:49 +0700

    yws

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,108 @@
-<div class="text-center">
-    <h1>Welcome to the Library</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, incidunt. Sit quos voluptas repellat, ipsum aut necessitatibus debitis illum sed non quod possimus quis tempore labore accusamus soluta? Maiores optio sed mollitia molestiae dolorem. Suscipit delectus doloribus provident quos recusandae culpa necessitatibus nobis architecto atque enim tempore, quis fugiat minima.</p>
-</div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Homepage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f8f8f8;
+            color: #333;
+        }
+
+        header {
+            background-color: #4285f4;
+            padding: 20px;
+            text-align: center;
+            color: #fff;
+        }
+
+        header h1 {
+            color: #fff;
+        }
+
+        .info-text {
+            max-width: 800px;
+            margin: 20px auto;
+            line-height: 1.6;
+            text-align: justify;
+        }
+
+        .features {
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            margin: 20px auto;
+        }
+
+        .feature {
+            width: 30%;
+            padding: 20px;
+            margin: 10px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            transition: transform 0.3s ease-in-out;
+        }
+
+        .feature:hover {
+            transform: scale(1.05);
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Library</h1>
+        <p>Explore, Discover, and Learn</p>
+    </header>
+
+    <section>
+        <div class="info-text">
+            <p>Welcome to the Library, your modern hub for knowledge and exploration. Immerse yourself in a diverse collection of books, discover new titles, and elevate your learning experience. Reserve your favorite books online and pick them up at your convenience.</p>
+            <p>Our flexible loan services empower you to borrow books and materials, ensuring you have access to the resources you need. Stay informed about the latest library information, events, and services.</p>
+        </div>
+
+        <div class="features">
+            <div class="feature" data-aos="fade-up">
+                <h2>Book Reservation</h2>
+                <p>Reserve your books online and pick them up at your convenience.</p>
+            </div>
+
+            <div class="feature" data-aos="fade-up" data-aos-delay="100">
+                <h2>Loan Services</h2>
+                <p>Borrow books and other materials for a specified period.</p>
+            </div>
+
+            <div class="feature" data-aos="fade-up" data-aos-delay="200">
+                <h2>Library Information</h2>
+                <p>Get details about our library, services, and events.</p>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Library Name. All rights reserved.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            AOS.init();
+        });
+    </script>
+</body>
+</html>
--- app/views/team/index.php
+++ app/views/team/index.php
@@ -1,37 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+      color: #333;
+      text-align: center;
+    }
 
+    #team-introduction {
+      background-color: #3498db;
+      padding: 50px 0;
+      color: #fff;
+    }
 
+    .team-member {
+      width: 200px; /* Set a fixed width for consistent size */
+      position: relative;
+      display: inline-block;
+      margin: 20px;
+      overflow: hidden;
+      border-radius: 15px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      transition: transform 0.3s ease-in-out;
+    }
+
+    .team-member img {
+      display: block;
+      width: 100%;
+      height: auto;
+      border-top-left-radius: 15px;
+      border-top-right-radius: 15px;
+    }
+
+    .team-member .member-info {
+      padding: 20px;
+      background-color: #fff;
+      box-sizing: border-box;
+    }
+
+    .team-member p {
+      margin: 10px 0;
+      font-size: 16px;
+      margin-bottom: 0;
+    }
+
+    .team-member:hover {
+      transform: translateY(-10px);
+    }
+
+    #main-content {
+      padding: 50px;
+    }
+
+    h3 {
+      color: #333;
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.6;
+      color: #555;
+    }
+  </style>
+  <title>Team Introduction</title>
+</head>
 <body>
-    <div class="container text-center">
-        <h1>Team Introduction</h1>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Team Member 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Team Member 1</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Team Member 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Team Member 2</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Team Member 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Team Member 3</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+  <section id="team-introduction">
+    <h2>Meet Our Team</h2>
+    <div class="team-member">
+      <img src="https://via.placeholder.com/150" alt="Azahra Salsabila">
+      <div class="member-info">
+        <p style="font-weight: bold;">Azahra Salsabila</p>
+        <p>Number 05</p>
+      </div>
     </div>
+    <div class="team-member">
+      <img src="https://via.placeholder.com/150" alt="Lenka Melinda Florienka">
+      <div class="member-info">
+        <p style="font-weight: bold;">Lenka Melinda</p>
+        <p>Number 12</p>
+      </div>
+    </div>
+    <div class="team-member">
+      <img src="https://via.placeholder.com/150" alt="Sri Kresna Maha Dewa">
+      <div class="member-info">
+        <p style="font-weight: bold;">Sri Kresna</p>
+        <p>Number 25</p>
+      </div>
+    </div>
+  </section>
+
+  <section id="main-content">
+    <h3>Welcome to Our Library</h3>
+    <p>Your modern hub for knowledge and exploration. Immerse yourself in a diverse collection of books, discover new titles, and elevate your learning experience. Reserve your favorite books online and pick them up at your convenience.</p>
+    
+  </section>
+
+  <script>
+    // Fade-in animation when the page loads
+    document.addEventListener("DOMContentLoaded", function () {
+      const fadeInElements = document.querySelectorAll(".team-member, #main-content");
+
+      fadeInElements.forEach((element) => {
+        element.style.opacity = 0;
+        element.style.transition = "opacity 1s ease-in-out";
+      });
+
+      let delay = 500;
+      fadeInElements.forEach((element) => {
+        setTimeout(() => {
+          element.style.opacity = 1;
+        }, delay);
+        delay += 500;
+      });
+    });
+  </script>
+
 </body>
 </html>
